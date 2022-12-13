9e004b33f1ab9ff2cda8a4f6fc765905d9d0924a
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-12-13 23:05:18 +0200

    add check double contcts functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,10 @@ import { nanoid } from 'nanoid';
 export class App extends Component {
 
   state = {
-    contacts: [],
+    contacts: [{ id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }],
     filter: '',
   };
 
@@ -20,6 +23,10 @@ export class App extends Component {
   };
 
   addContact = (data) => {
+    if (this.checkDoubleContact(data)) {
+      alert(`${data.name} is already in your contacts!`);
+      return;
+    }
     const newContact = {
       id: nanoid(),
       name: data.name,
@@ -28,6 +35,10 @@ export class App extends Component {
     this.setState(prevState => ({
       contacts: [newContact, ...prevState.contacts],
     }));
+  };
+
+  checkDoubleContact = (inputData) => {
+    return this.state.contacts.find(contact => contact.name === inputData.name);
   };
 
   getVisibleContacts = () => {
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,9 +5,10 @@ import { ContactItem } from '../ContactItem/ContactItem';
 export const ContactList = ({ contacts }) => {
   return (
     <ul>
-      {contacts.map(({ id, name, number }) => <ContactItem key={id}
-                                                           name={name}
-                                                           number={number} />)}
+      {contacts.map(({ id, name, number }) =>
+        <ContactItem key={id}
+                     name={name}
+                     number={number} />)}
     </ul>
   );
 };
