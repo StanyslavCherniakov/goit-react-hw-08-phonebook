cb81f553e5e41873ec0b767d99695a018dd03496
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-12-28 23:55:23 +0200

    refactor in hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
@@ -7,11 +7,15 @@ import LsService from './utils/localStorage';
 import { nanoid } from 'nanoid';
 import { Title, TitleContacts, Wrapper } from './App.styled';
 
-
 export const App = () => {
-
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState(() => {
+    return LsService.load('contacts') ?? [];
+  });
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    LsService.save('contacts', contacts);
+  }, [contacts]);
 
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
@@ -44,7 +48,6 @@ export const App = () => {
     setContacts(prevState => prevState.filter(el => el.id !== id));
   };
 
-
   return (
     (<Wrapper>
       <Title>Phonebook</Title>
@@ -54,78 +57,3 @@ export const App = () => {
       <ContactList contacts={getVisibleContacts()} deleteContact={deleteContact} />
     </Wrapper>));
 };
-
-
-// export class App extends Component {
-//
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-//
-//   componentDidMount() {
-//     if (LsService.load('phonebook')) {
-//       const LSData = LsService.load('phonebook');
-//       this.setState({ contacts: LSData });
-//     }
-//   }
-//
-//   componentDidUpdate(pProps, pState) {
-//     if (pState.contacts !== this.state.contacts) {
-//       LsService.save('phonebook', this.state.contacts);
-//     }
-//   }
-//
-//   handleInputChange = (e) => {
-//     this.setState({
-//       [e.currentTarget.name]: e.currentTarget.value,
-//     });
-//   };
-//
-//   addContact = (data) => {
-//     const { name, number } = data;
-//     if (this.checkDoubleContact(data)) {
-//       alert(`${name} is already in your contacts!`);
-//       return;
-//     }
-//     const newContact = {
-//       id: nanoid(),
-//       name,
-//       number,
-//     };
-//     this.setState(prevState => ({
-//       contacts: [newContact, ...prevState.contacts],
-//     }));
-//   };
-//
-//   checkDoubleContact = (inputData) => {
-//     return this.state.contacts.find(contact => contact.name === inputData.name);
-//   };
-//
-//   getVisibleContacts = () => {
-//     const { contacts, filter } = this.state;
-//     const normalized = filter.toLowerCase();
-//     return contacts.filter(contact => contact.name.toLowerCase().includes(normalized));
-//   };
-//
-//   deleteContact = (id) => {
-//     this.setState(prevState => ({
-//       contacts: prevState.contacts.filter(el => el.id !== id),
-//     }));
-//   };
-//
-//   render() {
-//     const visibleContacts = this.getVisibleContacts();
-//
-//     return (<Wrapper>
-//       <Title>Phonebook</Title>
-//       <ContactForm onSubmit={this.addContact} />
-//       <TitleContacts>Contacts</TitleContacts>
-//       <Filter value={this.state.filter} onFilter={this.handleInputChange} />
-//       <ContactList contacts={visibleContacts} deleteContact={this.deleteContact} />
-//     </Wrapper>);
-//   }
-// }
-
-
-
