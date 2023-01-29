5d4d2370d52f6e46711df1dde22a871a699ca12f
StanyslavCherniakov <stas.chernykov@gmail.com>
2023-01-29 21:08:22 +0200

    add store and slice for add/del contacts

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.push(action.payload);
+    },
+    removeContact: (state, action) => {
+      state.contacts.filter(el => el.id !== action.payload.id);
+    },
+  },
+});
