45d82fe61cd392d4a2d160c2494e3add54536781
StanyslavCherniakov <stas.chernykov@gmail.com>
2023-02-25 16:24:32 +0200

    add header styles all

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Button } from '@mui/material';
+import { NavItem } from '../Navigation/Navigation.styled';
 
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink to='/register'>
-        Register
-      </NavLink>
-      <NavLink to='/login'>
-        Log In
-      </NavLink>
+      <Button color='inherit'>
+        <NavItem to='/register'>
+          Register
+        </NavItem>
+      </Button>
+      <Button color='inherit'>
+        <NavItem to='/login'>
+          Log In
+        </NavItem>
+      </Button>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Box } from '@mui/material';
+
+import BackGroundImg
+  from '../images/Stanyslav_Cherniakov_background_for_contact_book_app_in_modern__a47f0727-d336-42c5-8f26-10597a5d32c7.png';
 
 export const HomePage = () => {
   return (
-    <div>
-      <p>HOME PAGE</p>
-    </div>
+    <Box
+      sx={{
+        backgroundImage: `url(${BackGroundImg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '100vh',
+      }}
+    >
+    </Box>
   );
 };
 
